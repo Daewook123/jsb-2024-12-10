687313906b3be6f94098fbc821579e8718a0b210
Daewook123 <cdo5080@gmail.com>
2024-12-12 16:32:50 +0900

    2장 완료

--- src/main/java/com/mysite/sbb/domain/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/domain/answer/controller/AnswerController.java
@@ -1,15 +1,17 @@
 package com.mysite.sbb.domain.answer.controller;
 
+import com.mysite.sbb.domain.answer.AnswerForm;
 import com.mysite.sbb.domain.answer.service.AnswerService;
 import com.mysite.sbb.domain.question.Question;
 import com.mysite.sbb.domain.question.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -20,9 +22,14 @@ public class AnswerController {
     private final QuestionService questionService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);// TODO: 답변을 저장한다.
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());// TODO: 답변을 저장한다.
         return String.format("redirect:/question/detail/%s", id);
     }
 
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,12 +1,18 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.domain.answer.Answer;
 import com.mysite.sbb.domain.question.Question;
 import com.mysite.sbb.domain.question.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -14,10 +20,17 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Transactional
 	@Test
 	void testJpa() {
-		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-		assertEquals(1, q.getId());
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 
 }
