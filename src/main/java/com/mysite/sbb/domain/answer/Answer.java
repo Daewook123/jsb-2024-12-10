32f4c17cd58226d5285f574c369bc4dd7e734645
Daewook123 <cdo5080@gmail.com>
2024-12-16 15:09:42 +0900

    앵커 기능 추가

--- src/main/java/com/mysite/sbb/domain/answer/Answer.java
+++ src/main/java/com/mysite/sbb/domain/answer/Answer.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -25,7 +26,10 @@ public class Answer {
 
     @ManyToOne
     private Question question;
-    @ManyToOne
 
+    @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
